8bc97de7d00724c172986a59839b825737c94755
nwaokocha michael <nwaokocham@gmail.com>
2018-11-04 12:05:23 +0100

    version 1 complete

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # documentary-finder
 A SINGLE PAGED APP THAT SEARCHES FOR DOCUMENTARIES ON YOUTUBE
+
+
+
+# HOW TO USE
+
+All you have to do is type in a the name of what you want to see a documentary on e.g **bread**, **sharks**, **worms**. then click the search button to get documentaries on it.
+
+
+# BUILT WITH
+ 
+* Html
+* css
+* Jquery
+* Youtube Api
+
+
+you can use the app [HERE](http://a.com)
---
+++ js/app.js
@@ -0,0 +1,86 @@
+var currentVid = '';
+var app = {
+    vidGet: function(text){
+     fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${text}documentary&type=video&videoEmbbeddable=true&maxResults=10&key=AIzaSyD7ZsC8QN-Puf1o_ISF40rA4myG9FmSe20`)
+        .then(res => res.json())
+        .then(data => this.vidCreate(data))
+        .catch(data => this.errorMessage());
+    },
+
+    vidCreate: function(data){
+        let videos = data.items;
+        let firstvid = videos[0].id.videoId;
+        currentVid = firstvid;
+        let vidContainer = $('.video-container');
+        let vid = `<iframe class="video-frame" id="${firstvid}" height="405" src="https://www.youtube.com/embed/${firstvid}" frameborder="0"  encrypted-media" allowfullscreen></iframe>`;
+        let title = `<h3>${videos[0].snippet.title}</h3>`
+        vidContainer.append(title);
+        vidContainer.append(vid);
+        this.vidList(videos);
+    },
+
+    errorMessage: function(){
+        let vidContainer = $('.video-container');
+        vidContainer.addClass('error');
+    },
+
+    vidList: function(data){
+        var array = data;
+        array.forEach(function(item, i){
+            let id = array[i].id.videoId;
+            let snippet = item.snippet;
+            let alternatives = $('.altList');
+            let thumbnail = snippet.thumbnails.default;
+            let img = `<li id="${id}"><img src="${thumbnail.url}" width="170" height="100">
+                        <p>${snippet.title}</p>            
+                        </li>`;
+            alternatives.append(img);
+        });
+        this.vidChange()
+    },
+
+    vidChange: function(){
+        let li = $('li');
+        li.click(function(event){
+            let id = this.id;
+            let p = this.querySelector('p');
+            if(id === currentVid){
+                //do nothing
+            }else{
+                let vidContainer = $('.video-container');
+                vidContainer.html(' ');
+                let vid = `<iframe class="video-frame" id="${id}" height="405" src="https://www.youtube.com/embed/${id}" frameborder="0"  encrypted-media" allowfullscreen></iframe>`;
+                let title = `<h3>${p.innerText}</h3>`
+                vidContainer.append(title);
+                vidContainer.append(vid);
+                currentVid = id;
+            }
+        })
+    },
+
+    init: function () {
+        let search = $('.searchBut');
+        search.click(function (e) {
+            e.preventDefault();
+            let searchText = $('#searchText').val();
+            let text = searchText.replace(/\s/g, '+');
+            if (searchText.length > 0) {
+                let vidContainer = $('.video-container');
+                vidContainer.html(' ');
+                vidContainer.removeClass('error');
+                let alternatives = $('.altList');
+                alternatives.html(' ');
+                app.vidGet(text);
+                app.vidChange();
+
+            } else {
+                //do nothing
+            }
+        })
+    },
+
+
+}
+
+
+app.init();
