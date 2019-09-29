cf3aa6b208ef3e9bf02d8535f2cf472ac0bdf2c0
nwaokocha michael <nwaokocham@gmail.com>
2019-09-30 00:11:57 +0100

    fixed code error

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 var currentVid = '';
 
 var app = {
-    vidGet: function(text){
+    vidGet: function (text) {
         fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${text}documentary&type=video&videoEmbbeddable=true&maxResults=10&key=AIzaSyAjX_OFeFwvPwGdf50i-yGqci6JduqTMcw`)
-        .then(res => res.json())
-        .then(data => this.vidCreate(data))
-        .catch(data => this.errorMessage());
+            .then(res => res.json())
+            .then(data => this.vidCreate(data))
+            .catch(data => this.errorMessage());
     },
 
-    vidCreate: function(data){
+    vidCreate: function (data) {
         let videos = data.items;
         let firstvid = videos[0].id.videoId;
         currentVid = firstvid;
@@ -20,14 +20,14 @@ var app = {
         this.vidList(videos);
     },
 
-    errorMessage: function(){
+    errorMessage: function () {
         let vidContainer = $('.video-container');
         vidContainer.addClass('error');
     },
 
-    vidList: function(data){
+    vidList: function (data) {
         var array = data;
-        array.forEach(function(item, i){
+        array.forEach(function (item, i) {
             let id = array[i].id.videoId;
             let snippet = item.snippet;
             let alternatives = $('.altList');
@@ -40,14 +40,14 @@ var app = {
         this.vidChange()
     },
 
-    vidChange: function(){
+    vidChange: function () {
         let li = $('li');
-        li.click(function(event){
+        li.click(function (event) {
             let id = this.id;
             let p = this.querySelector('p');
-            if(id === currentVid){
+            if (id === currentVid) {
                 //do nothing
-            }else{
+            } else {
                 let vidContainer = $('.video-container');
                 vidContainer.html(' ');
                 let vid = `<iframe class="video-frame" id="${id}" height="405" src="https://www.youtube.com/embed/${id}" frameborder="0"  encrypted-media" allowfullscreen></iframe>`;
@@ -61,11 +61,10 @@ var app = {
 
     init: function () {
         let search = $('.searchBut');
-        let tab = $('#searchText');
-
+        let doc = $('#searchText');
         search.click(function (e) {
             e.preventDefault();
-            let searchText = tab.val();
+            let searchText = $('#searchText').val();
             let text = searchText.replace(/\s/g, '+');
             if (searchText.length > 0) {
                 let vidContainer = $('.video-container');
@@ -81,10 +80,10 @@ var app = {
             }
         });
 
-        tab.addEventListener("keydown", function (e) {
-            e.preventDefault();
-            if( e.keycode === 13){
-                let searchText = tab.val();
+        doc.keypress(function (e) {
+            if (e.which === 13) {
+                e.preventDefault();
+                let searchText = $('#searchText').val();
                 let text = searchText.replace(/\s/g, '+');
                 if (searchText.length > 0) {
                     let vidContainer = $('.video-container');
@@ -94,17 +93,9 @@ var app = {
                     alternatives.html(' ');
                     app.vidGet(text);
                     app.vidChange();
-
-                } else {
-                    //do nothing
-                }
-            }else {
-                // do nothing
+                };
             }
-        })
-
-
-
+        });
     },
 
 
